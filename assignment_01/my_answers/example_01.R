9aff2126c0f03ff1b424f579fe1561c796500415
MelanieDanaeGarcia <mgarcia14@knights.ucf.edu>
2019-09-17 18:32:40 -0400

    example_01

---
+++ assignment_01/my_answers/example_01.R
@@ -0,0 +1,15 @@
+#Write an R function that reads two numbers, multiplies them together and prints out their
+#product.
+
+num_1 <- 10
+
+num_2 <- 5
+
+multiply_two <- function(num_1,num_2) 
+{
+  multi <- num_1*num_2
+  
+  return (multi)
+}
+
+multiply_two(num_1,num_2)

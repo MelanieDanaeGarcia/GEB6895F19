c08481714028e0d528998aebfb5504a39ec142c0
MelanieDanaeGarcia <mgarcia14@knights.ucf.edu>
2019-09-17 22:05:14 -0400

    example_05

---
+++ assignment_01/my_answers/example_05.R
@@ -0,0 +1,13 @@
+#Write an R function that will count all the even numbers up to a user defined stopping point.
+
+x_values <- seq(1,20)
+
+mult <- function(x)
+  
+{
+  even <- x*2
+  
+  return(even)
+}
+
+mult(x_values)

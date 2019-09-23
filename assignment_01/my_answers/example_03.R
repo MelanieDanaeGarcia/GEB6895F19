9ea91a9574d94c4ad0138e13717f8702432b0c7e
MelanieDanaeGarcia <mgarcia14@knights.ucf.edu>
2019-09-23 17:34:10 -0400

    Edited example_03

--- assignment_01/my_answers/example_03.R
+++ assignment_01/my_answers/example_03.R
@@ -3,25 +3,129 @@
 #red. If the number is between 20 and 30, write the word green. If it is any other number,
 #write that it is not a correct color option.
 
-colornum <- function(num_in)
-
-  num_in <- read.line(prompt="Enter a number: ").
+color_by_number <- function(num_in) {
   
-{
-  if (0>num_in<=10)
+  if (num_in == 1)
+    
+  {print("Blue")}
   
-  return("Blue")
-
-} else
+  else if (num_in == 2)
     
-{
-  if (10>num_in<=20)
-
-  return("Red")
-} else 
+  {print("Blue")}
+  
+  else if (num_in == 3)
     
-{
-  if (20<num_in<=30)
-      
-  return("Green")
+  {print("Blue")}
+  
+  else if (num_in == 4)
+    
+  {print("Blue")}
+  
+  else if (num_in == 5)
+    
+  {print("Blue")}
+  
+  else if (num_in == 6)
+    
+  {print("Blue")}
+  
+  else if (num_in == 7)
+    
+  {print("Blue")}
+  
+  else if (num_in == 8)
+    
+  {print("Blue")}
+  
+  else if (num_in == 9)
+    
+  {print("Blue")}
+  
+  else if (num_in == 10)
+    
+  {print("Blue")}
+  
+  else if (num_in == 11)
+    
+  {print("Red")}
+  
+  else if (num_in == 12)
+    
+  {print("Red")}
+  
+  else if (num_in == 13)
+    
+  {print("Red")}
+  
+  else if (num_in == 14)
+    
+  {print("Red")}
+  
+  else if (num_in == 15)
+    
+  {print("Red")}
+  
+  else if (num_in == 16)
+    
+  {print("Red")}
+  
+  else if (num_in == 17)
+    
+  {print("Red")}
+  
+  else if (num_in == 18)
+    
+  {print("Red")}
+  
+  else if (num_in == 19)
+    
+  {print("Red")}
+  
+  else if (num_in == 20)
+    
+  {print("Red")}
+  
+  else if (num_in == 21)
+    
+  {print("Green")}
+  
+  else if (num_in == 22)
+    
+  {print("Green")}
+  
+  else if (num_in == 23)
+    
+  {print("Green")}
+  
+  else if (num_in == 24)
+    
+  {print("Green")}
+  
+  else if (num_in == 25)
+    
+  {print("Green")}
+  
+  else if (num_in == 26)
+    
+  {print("Green")}
+  
+  else if (num_in == 27)
+    
+  {print("Green")}
+  
+  else if (num_in == 28)
+    
+  {print("Green")}
+  
+  else if (num_in == 29)
+    
+  {print("Green")}
+  
+  else if (num_in == 30)
+    
+  {print("Green")}
+  
+  else
+    
+  {print("This is not a correct color option.")}
 }
